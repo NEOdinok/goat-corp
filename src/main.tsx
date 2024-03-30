e3c7e9c5a22d2b045f716207418f7eca3a3afd3d
goshamartynovich <goshamartynovich@gmail.com>
2024-03-30 21:00:59 +0300

    feat: added RTK, redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "@/app/store";
 
 import { App } from "@/app";
 
@@ -7,6 +9,8 @@ const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
