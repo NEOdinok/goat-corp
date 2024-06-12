fca5bc604e62e098c4d29b1b3f6cf014966790e0
goshamartynovich <goshamartynovich@gmail.com>
2024-06-12 18:57:18 +0300

    feat: radix select

--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -1,3 +1,11 @@
+import { BaseSelect } from "@/shared/ui/select";
+
 export const AboutPage = () => {
-  return <div style={{ padding: "1rem" }}>About</div>;
+  return (
+    <div
+      style={{ padding: "1rem", display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <BaseSelect />
+    </div>
+  );
 };
---
+++ src/shared/ui/select/index.tsx
@@ -0,0 +1,47 @@
+import * as Select from "@radix-ui/react-select";
+import cn from "clsx";
+
+import styles from "./styles.module.css";
+
+type Props = {
+  className?: string;
+  variant?: "primary";
+};
+
+export const BaseSelect = ({ className, variant }: Props) => {
+  const classList = cn(styles.root, styles[`variant-${variant}`], className);
+
+  return (
+    <Select.Root>
+      <Select.Trigger className={cn(classList, styles.trigger)} aria-label="Size selector">
+        <Select.Value placeholder="SIZE" />
+        <Select.Icon />
+      </Select.Trigger>
+
+      <Select.Portal>
+        <Select.Content className={styles.content} side="bottom">
+          <Select.Viewport>
+            <Select.Group className={styles.group}>
+              <Select.Item className={styles.item} value="XL">
+                <Select.ItemText>XL</Select.ItemText>
+              </Select.Item>
+              <Select.Separator className={styles.separator} />
+              <Select.Item className={styles.item} value="L">
+                <Select.ItemText>L</Select.ItemText>
+              </Select.Item>
+              <Select.Separator className={styles.separator} />
+              <Select.Item className={styles.item} value="M">
+                <Select.ItemText>M</Select.ItemText>
+              </Select.Item>
+              <Select.Separator className={styles.separator} />
+              <Select.Item className={styles.item} value="S">
+                <Select.ItemText>S</Select.ItemText>
+              </Select.Item>
+            </Select.Group>
+          </Select.Viewport>
+          <Select.Arrow />
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
