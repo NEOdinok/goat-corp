d4d66e8d68aedc7848ee793b823fa20a219b4698
goshamartynovich <goshamartynovich@gmail.com>
2024-06-14 18:40:28 +0300

    feat: radio stuff

---
+++ src/shared/ui/radio/group/index.tsx
@@ -0,0 +1,13 @@
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import * as React from "react";
+
+import { cn } from "@/shared/lib";
+
+export const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return <RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />;
+});
+
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
