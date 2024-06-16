e9c62aacda39385b6c4e225a9715dfe144750c87
goshamartynovich <goshamartynovich@gmail.com>
2024-06-16 18:16:36 +0300

    placeholder imgs added to gallery

--- src/shared/ui/gallery/index.tsx
+++ src/shared/ui/gallery/index.tsx
@@ -1,6 +1,10 @@
 import { cn } from "@/shared/lib";
 
 import placeholderImg from "../../assets/images/product-placeholder.png";
+// import placeholder1 from "../../assets/images/shirt-1.jpg";
+// import placeholder2 from "../../assets/images/shirt-2.jpg";
+// import placeholder3 from "../../assets/images/shirt-3.jpg";
+// import placeholder4 from "../../assets/images/shirt-4.jpg";
 import styles from "./styles.module.css";
 
 export interface Props {
@@ -15,6 +19,7 @@ const images = [
     id: 0,
     title: "accusamus beatae ad facilis cum similique qui sunt",
     url: "https://via.placeholder.com/600/92c952",
+    // src: placeholder1,
     thumbnailUrl: "https://via.placeholder.com/150/92c952",
   },
   {
@@ -22,6 +27,7 @@ const images = [
     id: 1,
     title: "reprehenderit est deserunt velit ipsam",
     url: "https://via.placeholder.com/600/771796",
+    // src: placeholder2,
     thumbnailUrl: "https://via.placeholder.com/150/771796",
   },
   {
@@ -29,6 +35,7 @@ const images = [
     id: 2,
     title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
     url: "https://via.placeholder.com/600/d32776",
+    // src: placeholder3,
     thumbnailUrl: "https://via.placeholder.com/150/d32776",
   },
 ];
