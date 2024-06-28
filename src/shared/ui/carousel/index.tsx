0262a7883bbb56d0c173cade44efee467c1037f3
goshamartynovich <goshamartynovich@gmail.com>
2024-06-29 00:17:46 +0300

    feat: carousel, gallery show real photos

--- src/shared/ui/carousel/index.tsx
+++ src/shared/ui/carousel/index.tsx
@@ -232,18 +232,25 @@ const CarouselNext = React.forwardRef<HTMLButtonElement, React.ComponentProps<ty
 CarouselNext.displayName = "CarouselNext";
 
 interface Props {
-  className?: undefined | string;
+  className?: string | undefined;
+  imageUrls?: Array<string | undefined>;
+  productName?: string | undefined;
 }
 
-const BaseCarousel = ({ className }: Props) => {
+const BaseCarousel = ({ className, imageUrls, productName }: Props) => {
   const classList = cn(className);
 
   return (
     <Carousel className={classList}>
       <CarouselContent>
-        {mockProducts.map(({ url, src, title }) => (
+        {imageUrls?.map((url) => (
           <CarouselItem key={url}>
-            <img key={url} src={src} className="w-full aspect-[1/1] object-cover" alt={title} />
+            <img
+              key={url}
+              src={url}
+              className="w-full aspect-[1/1] object-cover"
+              alt={`${productName} + preview`}
+            ></img>
           </CarouselItem>
         ))}
       </CarouselContent>
--- src/shared/ui/gallery/index.tsx
+++ src/shared/ui/gallery/index.tsx
@@ -1,20 +1,26 @@
 import { cn } from "@/shared/lib";
-import { mockProducts } from "@/shared/lib/mock-products";
 
 export interface Props {
   className?: string | undefined;
+  imageUrls?: Array<string | undefined>;
+  productName?: string | undefined;
 }
 
 export type Ref = HTMLButtonElement;
 
-export const Gallery = ({ className }: Props) => {
+export const Gallery = ({ className, imageUrls, productName }: Props) => {
   const rootStyles = "";
   const classList = cn(rootStyles, className);
 
   return (
     <section aria-label="Image Gallery" className={classList}>
-      {mockProducts.map(({ url, title, src }) => (
-        <img key={url} src={src} className="w-full aspect-[1/1] object-cover" alt={title} />
+      {imageUrls?.map((url) => (
+        <img
+          key={url}
+          src={url}
+          className="w-full aspect-[1/1] object-cover"
+          alt={`${productName} + preview`}
+        />
       ))}
     </section>
   );
